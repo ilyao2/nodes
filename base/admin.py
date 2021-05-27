0b20787665366b5f7068b203a796c30845ce1ae2
Obydennye Ilya <ilya.obydennye@gmail.com>
2021-05-28 01:38:31 +0700

    base models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Graph)
+admin.site.register(Node)
+admin.site.register(Content)
+admin.site.register(Link)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    class Meta:
+        abstract = True
+
+    loginNSTU = models.CharField('Логин', max_length=200, unique=True, null=True)
+
+
+class Graph(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.User
+
+
+class Node(models.Model):
+    Graph = models.ForeignKey(Graph, on_delete=models.CASCADE)
+    Content = models.ForeignKey(Content, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Content
+
+
+class Content(User):
+    Text = models.TextField('Text')
+    Data = models.FileField('Data')
+    Ord = models.IntegerField('Ord')
+
+    def __str__(self):
+        return self.Text
+
+
+class Link(models.Model):
+    StartNode = models.ForeignKey(Node, on_delete=models.CASCADE)
+    EndNode = models.ForeignKey(Node, on_delete=models.CASCADE)
+    Weight = models.IntegerField('Вес')
+
+    def __str__(self):
+        return self.Weight
+
